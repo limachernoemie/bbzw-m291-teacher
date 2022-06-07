75e13aae1a21b663958cbbcca8aba523b1448ed4
liviawer22 <105320762+liviawer22@users.noreply.github.com>
2022-06-07 23:54:05 +0200

    Add files via upload

---
+++ assets/js/form.js
@@ -0,0 +1,30 @@
+const submit = document.getElementById("submit")
+const email = document.getElementById("email")
+const name = document.getElementById("name")
+submit.disabled = true
+
+const validate = () => {
+    if (email.value == "") {
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+}
+
+email.addEventListener("keyup", (event) => {
+    validate()
+})
+
+
+submit.addEventListener("click", async (event) => {
+    event.preventDefault()
+    const result = await databaseClient.insertInto("users", ["email"],["name"], [email.value])
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+       // location.href = "game.html"
+    }
+
+})
